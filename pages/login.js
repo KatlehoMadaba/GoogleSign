910019e45736d15d85f065648a58501259dc81b8
Oreutlwile Diutlwileng <oreutlwilediutlwileng@gmail.com>
2023-06-30 14:13:32 +0200

    4th Commit

--- pages/login.js
+++ pages/login.js
@@ -1,49 +1,90 @@
-import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, TextInput} from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 
 function Login() {
   const navigation = useNavigation();
 
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
   return (
-<SafeAreaView style={{flex: 1, backgroundColor: '#e8ecf4'}}>
-  <View style={styles.container}>
-    <View style={styles.title}>
-      <Text>Welcome to My App</Text>
-    </View>
+<View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Welcome back!</Text>
 
-    <View style={styles.form}>
-      <View style={styles.input}>
-        <Text style={styles.inputLabel}>Email Address</Text>
+          <Text style={styles.subtitle}>Sign in to your account</Text>
+        </View>
 
-        <TextInput
-        style={styles.inputControl}
-        placeholder='example@example.com'
-        placeholderTextColor='black'
-        // value={form.email}
-        // onChangeText={() => }
-        />
+        <View style={styles.form}>
+          <View style={styles.input}>
+            <Text style={styles.inputLabel}>Email address</Text>
+
+            <TextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="email-address"
+              onChangeText={email => setForm({ ...form, email })}
+              placeholder="john@example.com"
+              placeholderTextColor="#6b7280"
+              style={styles.inputControl}
+              value={form.email}
+            />
+          </View>
+
+          <View style={styles.input}>
+            <Text style={styles.inputLabel}>Password</Text>
+
+            <TextInput
+              autoCorrect={false}
+              onChangeText={password => setForm({ ...form, password })}
+              placeholder="********"
+              placeholderTextColor="#6b7280"
+              style={styles.inputControl}
+              secureTextEntry={true}
+              value={form.password}
+            />
+          </View>
+
+          <View style={styles.formAction}>
+            {/* <TouchableOpacity
+              onPress={() => navigation.navigate('Login', {category: 'holistic-focus'})}
+              >
+              <View style={styles.btn}>
+                <Text style={styles.btnText}>Sign in</Text>
+              </View>
+            </TouchableOpacity> */}
+            <TouchableOpacity
+          //style={styles.category}
+          onPress={() => navigation.navigate('Menu')}
+          >
+                <View style={styles.btn}>
+                <Text style={styles.btnText}>Sign in</Text>
+              </View>
+          </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Menu')}>
+            <Text style={styles.formFooter}>
+              Don't have an account?{' '}
+              <Text style={{ textDecorationLine: 'underline' }}>Sign up</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-
     </View>
-    <View style={styles.form}>
-      <View style={styles.input}>
-        <Text style={styles.inputLabel}>Password</Text>
-
-        <TextInput
-        style={styles.inputControl}
-        placeholder='############'
-        placeholderTextColor='black'
-        // value={form.email}
-        // onChangeText={() => }
-        />
-      </View>
-
-    </View>
-    
-  </View>
-</SafeAreaView>
   )
 }
 
@@ -51,40 +92,85 @@ export default Login
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
   },
-  header:{
+  header: {
     marginVertical: 36,
-
   },
-  title:{
-    fontSize: 28,
-    fontWeight: '700',
-    color: 'black',
+  form: {
+    marginBottom: 24,
+  },
+  formAction: {
+    marginVertical: 24,
+  },
+  formFooter: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#222',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#1d1d1d',
     marginBottom: 6,
     textAlign: 'center',
   },
-  input:{
-
+  subtitle: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#929292',
+    textAlign: 'center',
   },
-  inputLabel:{
-    fontSize: 12,
+  input: {
+    marginBottom: 16,
+  },
+  inputLabel: {
+    fontSize: 17,
     fontWeight: '600',
     color: '#222',
     marginBottom: 8,
   },
-  inputControl:{
-    backgroundColor: '#fff',
+  inputControl: {
     height: 44,
+    backgroundColor: '#f1f5f9',
     paddingHorizontal: 16,
     borderRadius: 12,
     fontSize: 15,
     fontWeight: '500',
-    color: '#222'
-
+    color: '#222',
   },
-  
+  btn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderWidth: 1,
+    backgroundColor: '#007aff',
+    borderColor: '#007aff',
+  },
+  btnText: {
+    fontSize: 17,
+    lineHeight: 24,
+    fontWeight: '600',
+    color: '#fff',
+  },
+  category:{
+    width: 150,
+    height: 150,
+    margin: 10,
+    borderRadius: 10,
+    backgroundColor: '#FFFFFF',
+    shadowColor: '#000000',
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+},
 });
